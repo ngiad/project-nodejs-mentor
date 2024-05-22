ecf3040aebaaf6b56dcd32aa4f1513b73800ad7e
ngiad <follownghia001@gmail.com>
2024-05-23 00:26:57 +0700

    css home page

--- routers/products.js
+++ routers/products.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
-import ProductsController from "../controllers/products.controller.js"
+import ProductsController from "../controllers/products.controller.js";
 import { wrap } from "../helper/wrapController.js";
+import products from "../json.js";
 
-const productsRouter = Router()
-const { getProducts, getProductDetail,searchProduct } = wrap(new ProductsController()) 
+const productsRouter = Router();
+const { getProducts, getProductDetail, searchProduct } = wrap(
+  new ProductsController()
+);
 
+function getTestProduct(req, res) {
+  res.json(
+    products.filter((item) =>  req.query.category ? item.category === req.query.category :  true)
+  );
+}
 
-productsRouter.get("/", getProducts)
-productsRouter.get("/search", searchProduct)
-productsRouter.get("/:id", getProductDetail)
+// productsRouter.get("/", getProducts)
+productsRouter.get("/", getTestProduct);
+productsRouter.get("/search", searchProduct);
+productsRouter.get("/:id", getProductDetail);
 
-export default productsRouter
+export default productsRouter;
